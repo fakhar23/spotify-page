75136ada728b6765831afbea9b1cb7d258c437d6
Muhammad Fakhar <muhammadfakhar111@gmail.com>
2022-11-22 14:30:19 +0500

    final with some unused css, about to remove it

---
+++ nav.js
@@ -0,0 +1,6 @@
+// make mobile nav work
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector("header");
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
